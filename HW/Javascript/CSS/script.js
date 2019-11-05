76e9604a144730f6e77778164025e33797382171
Tonyia Johnson <tonyia2017@gmail.com>
2019-11-04 19:37:29 -0500

    ugh

---
+++ HW/Javascript/CSS/script.js
@@ -0,0 +1,10 @@
+< script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" > < /script> <
+script type = "text/javascript" >
+	var tipsClicked = 0;
+$(document).ready(function() {
+			$("#tips").click(function() {
+				$("#tips").css("font-size", "60px");
+			});
+
+			<
+			/script>
