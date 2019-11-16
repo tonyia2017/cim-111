91426ffec3d48f106a41f761b26b531491964087
Tonyia Johnson <tonyia2017@gmail.com>
2019-11-16 01:32:37 -0500

    latest j query

--- HW/Javascript/CSS/script.js
+++ HW/Javascript/CSS/script.js
@@ -2,9 +2,14 @@
 script type = "text/javascript" >
 	var tipsClicked = 0;
 $(document).ready(function() {
-			$("#tips").click(function() {
-				$("#tips").css("font-size", "60px");
-			});
+		$("#tips").click(function() {
+			$("#tips").css("font-size", "60px");
+		});
+	})
 
-			<
-			/script>
+	<
+	/script>
+
+	<
+	script src = "//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js" > < /script> <
+script src = "//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js" > < /script>
